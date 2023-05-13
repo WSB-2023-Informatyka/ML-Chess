10c78d723aa89c76651046f8b342bfc0d6e30b84
Cezary Zdrojewski <zdrojewski.czarek@gmail.com>
2023-05-13 12:22:19 +0200

    feat(gui): added chesboard displaying
    Signed-off-by: Cezary Zdrojewski <zdrojewski.czarek@gmail.com>

--- GUI.py
+++ GUI.py
@@ -2,24 +2,25 @@ import pygame
 
 
 class GuidedUI:
-	def __init__(self, window_size: tuple, board_color: tuple, background_color: tuple):
-
+	def __init__(self, window_size: tuple, board_color: tuple, background_color: tuple, size_of_board_square: int):
+		self.size_of_board_square = size_of_board_square
 		self.window_size = window_size
 		self.board_color = board_color
 		self.background_color = background_color
 
-	def draw_chessboard(self, surface: pygame.surface.Surface, chessboard_color: tuple) -> None:
+	def draw_chessboard(self, surface: pygame.surface.Surface, chessboard_color: tuple, square_size: int) -> None:
 		"""
 		Draw chessboard on screen
 		"""
 		for i in range(0, 8):
 			for j in range(0, 8):
 				if (i + j) % 2 == 0:
-					pygame.draw.rect(surface, chessboard_color, (i * 87, j * 87, 87, 87))
+					pygame.draw.rect(surface, chessboard_color,
+									 (i * square_size, j * square_size, square_size, square_size))
 
 	def run(self) -> None:
 		"""
-		Start main GUI process that updates screen.
+		Start main GUI process that updates screen
 		"""
 		running = True
 		pygame.display.set_caption("ML CHESS")
@@ -42,7 +43,7 @@ class GuidedUI:
 
 			pygame.display.flip()
 
-			self.draw_chessboard(back_buffer, self.board_color)
+			self.draw_chessboard(back_buffer, self.board_color, self.size_of_board_square)
 
 			screen.blit(back_buffer, (0, 0))
 
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import GUI
 
 
 def main() -> None:
-	
-	WINDOW_SIZE, WHITE, BLACK = (700, 700), (255, 255, 255), (0, 0, 0)
 
-	start = GUI.GuidedUI(WINDOW_SIZE, BLACK, WHITE)
+	WINDOW_SIZE, WHITE, BLACK = (700, 700), (255, 255, 255), (0, 0, 0)
+	SIZE_OF_BOARD_SQUARE = 87
+	start = GUI.GuidedUI(WINDOW_SIZE, BLACK, WHITE, SIZE_OF_BOARD_SQUARE)
 	start.run()
 
 
