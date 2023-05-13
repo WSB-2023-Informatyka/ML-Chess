12e316a04d9d0084b5cce67800f4ac9df7d7e87d
Cezary Zdrojewski <zdrojewski.czarek@gmail.com>
2023-05-13 16:03:08 +0200

    feat(gui): added chesboard displaying

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import GUI
 def main() -> None:
 
 	WINDOW_SIZE, WHITE, BLACK = (700, 700), (255, 255, 255), (0, 0, 0)
-	SIZE_OF_BOARD_SQUARE = 87
+	SIZE_OF_BOARD_SQUARE = 87 # should those constants be in capital letters?
 
 	start = GUI.GuidedUI(WINDOW_SIZE, BLACK, WHITE, SIZE_OF_BOARD_SQUARE)
 	start.run()
