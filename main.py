5304ffdce571c9ec0e0029ac2d42c39400e1baf8
Cezary Zdrojewski <zdrojewski.czarek@gmail.com>
2023-05-14 19:40:43 +0200

    fix(gui): adding requirements file and fixing variable names from Uppercase to lowercase

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import GUI
 
 
 def main() -> None:
+	window_size, white, black = (700, 700), (255, 255, 255), (0, 0, 0)
+	size_of_board_square = 87
 
-	WINDOW_SIZE, WHITE, BLACK = (700, 700), (255, 255, 255), (0, 0, 0)
-	SIZE_OF_BOARD_SQUARE = 87 # should those constants be in capital letters?
-
-	start = GUI.GuidedUI(WINDOW_SIZE, BLACK, WHITE, SIZE_OF_BOARD_SQUARE)
+	start = GUI.GuidedUI(window_size, black, white, size_of_board_square)
 	start.run()
 
 
